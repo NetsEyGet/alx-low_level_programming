b443a9abc85846437e11e876a0fd1037d633d50e
Netsanet <eliyabeyob24@gmail.com>
2022-12-10 04:06:28 -0800

    Done

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,11 +2,13 @@
  * File: 5-print_numbers.c
  * Auth: Brennan D Baran
  */
+
 #include <stdio.h>
- /**
-  * main - Prints all single digit numbers of base 10 starting from 0.
-  *
-  * Return: Always 0.
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
