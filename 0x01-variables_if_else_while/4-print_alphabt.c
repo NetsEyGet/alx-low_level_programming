b50bf29a4ff58e998fa756db541e8fb81ca917a4
Netsanet <eliyabeyob24@gmail.com>
2022-12-10 03:12:54 -0800

    Done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,9 +13,11 @@
 int main(void)
 {
 char letter;
+
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-if (letter != 'e' && letter != 'q')									putchar(letter);
+if (letter != 'e' && letter != 'q')							
+putchar(letter);
 }
 
 putchar('\n');
