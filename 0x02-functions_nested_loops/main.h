e71769beee5bb216e8779097350b261bf41dc644
Netsanet <eliyabeyob24@gmail.com>
2022-12-13 09:46:05 -0800

    TaskDone

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,6 +8,4 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
-
-#endif	
+int add(int, int);	
