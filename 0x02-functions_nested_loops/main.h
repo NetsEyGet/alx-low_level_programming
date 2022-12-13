74c9646338cb7a2730bd4825a90c9d9a0ded62d5
Netsanet <eliyabeyob24@gmail.com>
2022-12-13 06:26:02 -0800

    Done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,11 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _isalpha(int c);
 int _islower(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
 
 #endif	
