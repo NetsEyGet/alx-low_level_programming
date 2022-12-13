8c9a7fbd1d5f7bdc5a9f8097ffcfebf544febd98
Netsanet <eliyabeyob24@gmail.com>
2022-12-13 13:06:02 -0800

    Done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - prints the string "Holberton" from a character array.
+ * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
 */
 int main(void)
