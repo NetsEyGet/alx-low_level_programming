823fe3e057a1663aaa1ac07d20b88696716a4752
Netsanet <eliyabeyob24@gmail.com>
2022-12-19 06:11:42 -0800

    Done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -23,15 +23,14 @@ len++;
 while (i < len && f == 0)
 {
 if (s[i] == '-')
-
 ++d;
+
 if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
 if (d % 2)
-										digit = -digit;
-n = n * 10 + digit;
-										f = 1;
+digit = -digit;
+n = n * 10 + digit;										f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
 f = 0;
@@ -39,7 +38,6 @@ f = 0;
 i++;
 }
 if (f == 0)
-
 return (0);
 
 return (n);
