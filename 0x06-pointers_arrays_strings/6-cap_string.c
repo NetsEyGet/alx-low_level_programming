9633fd6a76f81a6f05670919380560f03654ef0c
Netsanet <eliyabeyob24@gmail.com>
2022-12-22 00:51:06 -0800

    Done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,36 @@
 #include "main.h"
-
 /**
- * cap_string - a function that capitalizes all words of a string
- * @n: input string
- * Return: caps on first letter of a separator
+ * cap_string - capitalizes most of the words in a string.
+ * @s: analized string.
+ *
+ * Return: String with all words capitalized.
  */
-char *cap_string(char *n)
+ char *cap_string(char *s)
 {
-int i, x;
-int cap = 32;
-int separators[] = {',', ';', '.', '?', '"',
-                    '(', ')', '{', '}', ' ', '\n', '\t'};
+int i, j;
+int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-for (i = 0; n[i] != '\0'; i++)
+i = 0;
+while (*(s + i) != '\0')
 {
-if (n[i] >= 'a' && n[i] <= 'z')
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
 {
-n[i] = n[i] - cap;
-}
-
-cap = 0;
-
-for (x = 0; x <= 12; x++)
+if (i == 0)
 {
-if (n[i] == separators[x])
+*(s + i) = *(s + i) - 32;
+}
+else
 {
-x = 12;
-cap = 32;
+for (j = 0; j <= 12; j++)
+{
+if (a[j] == *(s + i - 1))
+{
+*(s + i) = *(s + i) - 32;
 }
 }
 }
-return (n);
+}
+i++;
+}
+return (s);
 }
