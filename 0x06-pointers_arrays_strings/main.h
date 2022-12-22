4d93e0715b6b676b4ec4089449ff744bbc1b8b75
Netsanet <eliyabeyob24@gmail.com>
2022-12-22 03:26:12 -0800

    Done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,6 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *n);
-char *cap_string(char *n);
+char *cap_string(char *s);
 char *leet(char *n);
 char *rot13(char *n);
