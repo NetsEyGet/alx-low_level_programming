c90438b050e9ff8368c3e62a04ca6522b3c73f11
Netsanet <eliyabeyob24@gmail.com>
2023-01-04 08:13:57 -0800

    Done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,13 +8,13 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
-int sqrt2(int a, int b)
+int sqrt2(int x, int y)
 {
-if (b * b == a)
-return (b);
-else if (b * b > a)
+if (y * y == b)
+return (y);
+else if (y * y > x)
 return (-1);
-return (sqrt2(a, b + 1));
+return (sqrt2(x, y + 1));
 }
 /**
 * _sqrt_recursion - returns the natural square root of n
@@ -23,7 +23,7 @@ return (sqrt2(a, b + 1));
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
-int _sqrt_recursion(int a)
+int _sqrt_recursion(int n)
 {
-return (sqrt2(a, 1));
+return (sqrt2(n, 1));
 }
