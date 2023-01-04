839b03dea1d8c8a8d5df8f7b26c75d0e84de78e5
Netsanet <eliyabeyob24@gmail.com>
2023-01-04 05:22:41 -0800

    Done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,20 +9,20 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int wildcmp(char *s1, char *s2)
+int wildcmp(char *n1, char *n2)
 {
-if (*s1 == '\0' && *s2 == '\0')
+if (*n1 == '\0' && *n2 == '\0')
 return (1);
 
-if (*s1 == *s2)
-return (wildcmp(s1 + 1, s2 + 1));
+if (*n1 == *n2)
+return (wildcmp(n1 + 1, n2 + 1));
 
-if (*s2 == '*')
-						{
-if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-										return (0);
-										if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-										return (1);
+if (*n2 == '*')
+{
+if (*n2 == '*' && *(n2 + 1) != '\0' && *n1 == '\0')
+return (0);
+if (wildcmp(n1, n2 + 1) || wildcmp(n1 + 1, n2))
+return (1);
 										}
 return (0);
 }
