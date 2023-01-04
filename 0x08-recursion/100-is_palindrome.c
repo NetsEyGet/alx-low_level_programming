45a4162d855bbaa5c054b08cfda73e4c12643138
Netsanet <eliyabeyob24@gmail.com>
2023-01-04 07:21:59 -0800

    Done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* palind2 - obtains length of a
-* @a: string
-* @l: integer to count length
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+ * palind2 - obtains length of a
+ * @a: string
+ * @l: integer to count length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
 */
 int palind2(char *a, int l)
 {
@@ -15,12 +15,12 @@ return (l - 1);
 return (palind2(a + 1, l + 1));
 }
 /**
-* palind3 - compares string vs string reverse
-* @a: string
-* @l: length
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
 */
 
 int palind3(char *a, int l)
