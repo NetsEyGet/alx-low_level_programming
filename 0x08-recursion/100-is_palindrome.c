3b0b6b893f3a02be255ab3f2461b11af23681c42
Netsanet <eliyabeyob24@gmail.com>
2023-01-04 07:31:56 -0800

    Done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,7 +7,7 @@
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
-*/
+ */
 int palind2(char *a, int l)
 {
 if (*a == 0)
@@ -21,7 +21,7 @@ return (palind2(a + 1, l + 1));
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
-*/
+ */
 
 int palind3(char *a, int l)
 {
