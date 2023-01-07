f8945263355bc0002b94173005d08563f31bc2cb
Netsanet <eliyabeyob24@gmail.com>
2023-01-07 01:13:16 -0800

    Done

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -14,10 +14,10 @@ char *_strchr(char *s, char c)
 while (*s != '\0')
 {
 if (*s == c)
-										return (s);
+return (s);
 else if (*(s + 1) == c)
-										return (s + 1);
+return (s + 1);
 s++;
-										}
+}
 return (s + 1);
 }
