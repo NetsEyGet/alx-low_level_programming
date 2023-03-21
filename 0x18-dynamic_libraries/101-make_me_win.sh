9bec265f1313b357f8cb75484e562d2e8acddcbb
Netsanet <eliyabeyob24@gmail.com>
2023-03-21 00:30:12 -0700

    Done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+wget -O /tmp/win.so https://github.com/NetsEyGet/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lib101-makemewin.so
 export LD_PRELOAD=/tmp/win.so
